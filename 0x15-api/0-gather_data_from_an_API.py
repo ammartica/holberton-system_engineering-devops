dacc123d6dc7dec12b29e8b51f353567365d85f4
ammartica <amisadaymartinez@gmail.com>
2022-05-17 00:29:45 +0000

    removed name variable and exchanged it to getting it in the print itself

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -2,26 +2,21 @@
 """ Script that, using a REST API for a given employee ID,
 returns information about his/her TODO list progress """
 
-import json
 import requests
 from sys import argv
 
 
 if __name__ == "__main__":
-    emp_id = argv[1]
-    user = requests.get("https://jsonplaceholder.typicode.com/users/{}"
-                        .format(emp_id)).json()
-    todos = requests.get("https://jsonplaceholder.typicode.com/todos?userId={}"
-                         .format(emp_id)).json()
-    name = user.get('name')
+    user_id = argv[1]
+    user = requests.get("https://jsonplaceholder.typicode.com/users/{}".
+                        format(user_id)).json()
+    todo = requests.get("https://jsonplaceholder.typicode.com/todos?userId={}".
+                        format(user_id)).json()
     comp_tasks = []
-
-    for task in todos:
+    for task in todo:
         if task.get('completed') is True:
             comp_tasks.append(task.get('title'))
-
-    print("Employee {} is done with tasks({}/{}):"
-          .format(name, len(comp_tasks), len(todos)))
-
+    print("Employee {} is done with tasks({}/{}):".
+          format(user.get('name'), len(comp_tasks), len(todo)))
     for task in comp_tasks:
         print("\t {}".format(task))
