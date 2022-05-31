c5a950712a5f3db15f01f0dcb6fb89c6c51b1e2f
ammartica <amisadaymartinez@gmail.com>
2022-05-31 19:34:41 +0000

    made url variable more efficient

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -7,12 +7,10 @@ import requests
 
 def number_of_subscribers(subreddit):
     """returns total number of subscribers per given subreddit"""
-    url = "https://www.reddit.com/r/"
+    url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
     headers = {"User-Agent": "User Agent"}
-    response = requests.get(url + subreddit + "/about.json",
-                            headers=headers, allow_redirects=False)
+    response = requests.get(url, headers=headers, allow_redirects=False)
     if response.status_code == 200:
-        sub = response.json().get("data").get("subscribers")
         return response.json().get("data").get("subscribers")
     else:
         return 0
