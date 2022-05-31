32f92794f021b366f0845a91c17038180a5baf74
ammartica <amisadaymartinez@gmail.com>
2022-05-31 16:35:56 +0000

    add file

---
+++ 0x16-api_advanced/0-subs.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""Function that queries the Reddit API and returns the number of
+subscribers for a given subreddit"""
+import json
+import requests
+
+
+def number_of_subscribers(subreddit):
+    """returns the number of subscribers of given subreddit"""
+    url = "https://www.reddit.com/r/"
+    headers = {"User-Agent": "User Agent"}
+    response = requests.get(url + subreddit + "/about.json",
+                            headers=headers, allow_redirects=False)
+    if response.status_code == 200:
+        sub = response.json().get("data").get("subscribers")
+        return response.json().get("data").get("subscribers")
+    else:
+        return 0
